4fd2fbccca5a63688f3d0996fb1517972b83bec6
Juan Carlos Del Rio <jcdre@hotmail.com>
2021-04-07 20:54:44 -0500

    init commit, config, routes, CRUD

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const { config } = require('./config/index');
+
+const productsApi = require('./routes/products');
+
+productsApi(app);
+
+app.listen(config.port, function () {
+  console.log(`Listening http:localhost:${config.port}`);
+});
---
+++ utils/mocks/products.js
@@ -0,0 +1,86 @@
+const productsMock = [
+  {
+    id: 'bdf96406-6bd1-4fc2-84b6-e5b09f60df57',
+    name: 'Foie Gras',
+    size: 'S',
+    description:
+      'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.',
+    image: 'http://dummyimage.com/100x100.png/dddddd/000000',
+  },
+  {
+    id: 'edb53660-2e9a-46ec-ac70-91b934b22d19',
+    name: 'Bread - French Stick',
+    size: '3XL',
+    description:
+      'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.',
+    image: 'http://dummyimage.com/115x100.png/5fa2dd/ffffff',
+  },
+  {
+    id: 'aba1f117-93f4-4f51-8170-d684eb8b4f46',
+    name: 'Mix - Cocktail Strawberry Daiquiri',
+    size: 'S',
+    description:
+      'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+    image: 'http://dummyimage.com/135x100.png/ff4444/ffffff',
+  },
+  {
+    id: 'c65b83d6-9033-4319-9186-8a7e6cf317dd',
+    name: 'Garlic - Peeled',
+    size: 'M',
+    description:
+      'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+    image: 'http://dummyimage.com/172x100.png/5fa2dd/ffffff',
+  },
+  {
+    id: '623b8160-2b47-480a-9b41-3feca5731a1e',
+    name: 'Veal - Shank, Pieces',
+    size: 'L',
+    description:
+      'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
+    image: 'http://dummyimage.com/208x100.png/ff4444/ffffff',
+  },
+  {
+    id: 'f4fdaf36-fac9-4b7e-a9bf-75514f2d3949',
+    name: 'Nut - Pecan, Halves',
+    size: 'L',
+    description:
+      'Phasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+    image: 'http://dummyimage.com/246x100.png/5fa2dd/ffffff',
+  },
+  {
+    id: '85f11c2a-3f0e-40f4-b564-6b2509153d39',
+    name: 'Octopus',
+    size: 'M',
+    description:
+      'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    image: 'http://dummyimage.com/141x100.png/cc0000/ffffff',
+  },
+  {
+    id: 'ccb25f09-aa7b-46d5-9d77-5c495aa1d489',
+    name: 'Carbonated Water - Blackberry',
+    size: 'XL',
+    description:
+      'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.',
+    image: 'http://dummyimage.com/100x100.png/cc0000/ffffff',
+  },
+  {
+    id: '5ab5102d-e168-45ca-9c52-e7f5f8cd9a12',
+    name: 'Skirt - 29 Foot',
+    size: '2XL',
+    description:
+      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    image: 'http://dummyimage.com/208x100.png/dddddd/000000',
+  },
+  {
+    id: '24140d83-0178-4b34-ba29-ee1e7a4d6a5a',
+    name: 'Cup Translucent 9 Oz',
+    size: '3XL',
+    description:
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    image: 'http://dummyimage.com/155x100.png/dddddd/000000',
+  },
+];
+
+module.exports = {
+  productsMock,
+};
